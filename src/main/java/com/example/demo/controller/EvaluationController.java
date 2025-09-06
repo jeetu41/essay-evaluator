87c9ae916ccaede2dd281d2d4ab755b9f84ae9c3
Jitendra_Github <92706263+jeetu41@users.noreply.github.com>
2025-09-07 02:32:32 +0530

    EvaluationController.java

--- src/main/java/com/example/demo/controller/EvaluationController.java
+++ src/main/java/com/example/demo/controller/EvaluationController.java
@@ -1,9 +1,12 @@
+
 package com.example.demo.controller;
 
 import com.example.demo.service.AiEvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Map;
 
+import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class EvaluationController {
